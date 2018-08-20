238e12983b8741fdde9e233ad913b1ebe8656899
stan <stan@otbafrica.com>
2018-08-20 14:39:38 +0300

    Added google authenticator functionality

--- authapp/resources.py
+++ authapp/resources.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils import six
+from django.contrib.auth.models import User
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_text
+from django.http import HttpResponse
+from django.contrib.auth import login as auth_login
+from django.shortcuts import redirect
 
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
@@ -15,10 +21,10 @@ def activate(request, uidb64, token):
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
     if user is not None and account_activation_token.check_token(user, token):
+        print('here')
         user.is_active = True
+        user.email = ''
         user.save()
-        login(request, user)
-        # return redirect('home')
-        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+        return redirect('/account/two_factor/setup/')
     else:
         return HttpResponse('Activation link is invalid!')
